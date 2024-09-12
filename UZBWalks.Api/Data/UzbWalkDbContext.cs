534e827056c0e15a4cd277b3a3174d2084415b14
BehruzbekUmarov <bekhruz476@gmail.com>
2024-09-11 20:54:06 -0800

    Add project files.

---
+++ UZBWalks.Api/Data/UzbWalkDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UZBWalks.Api.Models.Domain;
+
+namespace UZBWalks.Api.Data
+{
+    public class UzbWalkDbContext : DbContext
+    {
+        public UzbWalkDbContext(DbContextOptions<UzbWalkDbContext> options):base(options)
+        { }
+
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+    }
+}
---
+++ UZBWalks.Api/Models/Domain/Difficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace UZBWalks.Api.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
