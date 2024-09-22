40fbc1faa6e06c5cd54740c248a5ba130475310a
BehruzbekUmarov <bekhruz476@gmail.com>
2024-09-21 21:00:29 -0800

    RegionUI actions is added

--- UZBWalks.UI/Controllers/RegionsController.cs
+++ UZBWalks.UI/Controllers/RegionsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
 using UZBWalks.UI.Models.DTO;
 
 namespace UZBWalks.UI.Controllers
@@ -12,6 +14,7 @@ namespace UZBWalks.UI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             List<RegionDto> response = new List<RegionDto>();
@@ -35,6 +38,89 @@ namespace UZBWalks.UI.Controllers
             }
 
             return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add() 
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel addRegionView)
+        {
+            var client = _httpClientFactory.CreateClient("RegionsClient");
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7054/api/Regions"),
+                Content = new StringContent(JsonSerializer.Serialize(addRegionView), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+
+            if(response is not null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var client = _httpClientFactory.CreateClient("RegionsClient");
+
+            var response = await client.GetFromJsonAsync<RegionDto>($"api/Regions/{id.ToString()}");
+
+            if(response is not null) return View(response);
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RegionDto regionDto)
+        {
+            var client = _httpClientFactory.CreateClient("RegionsClient");
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"https://localhost:7054/api/Regions/{regionDto.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(regionDto), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+
+            if (response is not null) return RedirectToAction("Index", "Regions");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDto regionDto)
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient("RegionsClient");
+
+                var httpRequestMessage = await client.DeleteAsync($"https://localhost:7054/api/Regions/{regionDto.Id}");
+
+                return RedirectToAction("Index", "Regions");
+            }
+            catch (Exception)
+            {
+                //Console
+            }
+
+            return View("Edit");
         }
     }
 }
